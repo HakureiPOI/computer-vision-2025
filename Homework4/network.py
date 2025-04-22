c25b08aadeb7d90f23d067f90073242c72ba3213
HakureiPOI <114349635+HakureiPOI@users.noreply.github.com>
2025-04-22 19:04:43 +0800

    Update and rename network_blank.py to network.py
    update

---
+++ Homework4/network.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2) 
+        self.dropout = nn.Dropout(0.25)
+        self.fc1 = nn.Linear(64 * 8 * 8, 128)
+        self.fc2 = nn.Linear(128, 10)  # CIFAR-10 共10类
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))     
+        x = self.pool(F.relu(self.conv2(x))) 
+        x = self.dropout(x)
+        x = x.view(-1, 64 * 8 * 8)     
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)              
+        return x
